3e75e145d5e760a67d91b656496006806c61069d
supermin45 <1299657352@qq.com>
2017-08-10 16:30:17 +0000

    printLyrics

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,22 +9,44 @@ chai.use(sinonChai);
 var main = require("../lib/main.js");
 
 
-describe("测试描述", function(){
+describe("Lyrics", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("returns the string when the given number is 1", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(1);
+        var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n'+
+'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+'No more bottles of beer on the wall, no more bottles of beer.\n'+
+'Go to the store and buy some more, 99 bottles of beer on the wall.';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+    it("returns the string when the given number is greater than 1 and less than 99", function(){
+        var result = main(10);
+        var expect_string = `10 bottles of beer on the wall, 10 bottles of beer.
+Take one down and pass it around, 9 bottles of beer on the wall.
+9 bottles of beer on the wall, 9 bottles of beer.
+Take one down and pass it around, 8 bottles of beer on the wall.
+8 bottles of beer on the wall, 8 bottles of beer.
+Take one down and pass it around, 7 bottles of beer on the wall.
+7 bottles of beer on the wall, 7 bottles of beer.
+Take one down and pass it around, 6 bottles of beer on the wall.
+6 bottles of beer on the wall, 6 bottles of beer.
+Take one down and pass it around, 5 bottles of beer on the wall.
+5 bottles of beer on the wall, 5 bottles of beer.
+Take one down and pass it around, 4 bottles of beer on the wall.
+4 bottles of beer on the wall, 4 bottles of beer.
+Take one down and pass it around, 3 bottles of beer on the wall.
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
 
         expect(expect_string).to.equal(result);
     });
